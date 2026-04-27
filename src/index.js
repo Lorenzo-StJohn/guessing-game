338e465ad07211270147535f8098e9c2c6cfbd0e
Lorenzo-StJohn <britishaccent@bk.ru>
2026-04-27 10:32:31 +0400

    refactor: move solution to the new file

--- src/index.js
+++ src/index.js
@@ -1,16 +1,27 @@
 class GuessingGame {
   constructor() {
-    throw new Error('Not implemented');
+    this.minV = 1;
+    this.maxV = 1;
+    this.midV = 1;
   }
-  /*
-  setRange(min, max) { }
 
-  guess() { }
+  setRange(min, max) {
+    this.minV = min + 1;
+    this.maxV = max - 1;
+  }
 
-  lower() { }
+  guess() {
+    this.midV = Math.ceil((this.minV + this.maxV) / 2);
+    return this.midV;
+  }
 
-  greater() { }
-*/
+  lower() {
+    this.maxV = this.midV - 1;
+  }
+
+  greater() {
+    this.minV = this.midV + 1;
+  }
 }
 
 module.exports = GuessingGame;
