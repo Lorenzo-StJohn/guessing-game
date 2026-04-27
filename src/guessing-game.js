9498e8f8f3abf985b66cdbbca0ce4a5e2deca4fb
Lorenzo-StJohn <britishaccent@bk.ru>
2026-04-27 10:28:35 +0400

    refactor: apply linter

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,28 +1,27 @@
 class GuessingGame {
-    constructor() {
-        this.minV = 1;
-        this.maxV = 1;
-        this.midV = 1;
-    }
+  constructor() {
+    this.minV = 1;
+    this.maxV = 1;
+    this.midV = 1;
+  }
 
-    setRange(min, max) {
-        this.minV = min + 1;
-        this.maxV = max - 1;
-    }
+  setRange(min, max) {
+    this.minV = min + 1;
+    this.maxV = max - 1;
+  }
 
-    guess() {
-        this.midV = Math.ceil((this.minV + this.maxV) / 2);
-        return this.midV;
-    }
+  guess() {
+    this.midV = Math.ceil((this.minV + this.maxV) / 2);
+    return this.midV;
+  }
 
-    lower() {
-        this.maxV = this.midV - 1;
-    }
+  lower() {
+    this.maxV = this.midV - 1;
+  }
 
-    greater() {
-        this.minV = this.midV + 1;
-    }
-
+  greater() {
+    this.minV = this.midV + 1;
+  }
 }
 
 module.exports = GuessingGame;
